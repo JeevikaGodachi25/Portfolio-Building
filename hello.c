a1de15750110aa77309fc56bbc6a9ed29480dd50
JeevikaGodachi25 <jeevikagodachi06@gmail.com>
2026-09-03 09:43:38 +0530

    Add greet() function --paired with <Jyoti Netrakar> via Live share

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+void greet(const char *name);
 int main(){
     printf("Hello World !");
+    greet("Jeevika");
     return 0;
+}
+void greet(const char *name) {
+    printf("Hello, %s! Welcome to your GitHub portfolio.\n", name);
+
 }
